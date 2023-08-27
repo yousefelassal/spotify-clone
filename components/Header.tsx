13a12d79d98869761c1b22aa648d728472ba4950
Yousef <yousefsameh24@gmail.com>
2023-08-27 16:59:59 +0300

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,10 @@
 import { useRouter } from "next/navigation"
 import { twMerge } from "tailwind-merge"
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+
+import Button from "./Button"
 
 export default function Header({children, className}: {children: React.ReactNode, className?: string}) {
   const router = useRouter()
@@ -31,6 +35,23 @@ export default function Header({children, className}: {children: React.ReactNode
                 >
                     <RxCaretRight size={35} className="text-white" />
                 </button>
+            </div>
+            <div className="flex md:hidden gap-x-2 items-center">
+                <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+                    <HiHome size={20} className="text-black" />
+                </button>
+                <button className="rounded-full p-2 bg-white items-center justify-center hover:opacity-75 transition">
+                    <BiSearch size={20} className="text-black" />
+                </button>
+            </div>
+            <div className="flex justify-between items-center gap-x-4">
+                <>
+                    <div>
+                        <Button>
+                            Sign up
+                        </Button>
+                    </div>
+                </>
             </div>
         </div>
     </div>
