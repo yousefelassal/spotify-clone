5b2087440cca209367b6df3793b5ac6f5df3923f
Yousef <yousefsameh24@gmail.com>
2023-08-27 19:08:42 +0300

    create list item component

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,5 +1,32 @@
-export default function ListItem() {
+'use client'
+
+import Image from "next/image";
+import { useRouter } from "next/navigation"
+import { FaPlay } from "react-icons/fa"
+
+export default function ListItem({image, name, href}:{image: string, name: string, href: string}) {
+  const router = useRouter()
+
+  const onClick = () => {
+    //add authentication
+    router.push(href);
+  }
   return (
-    <div>ListItem</div>
+    <button className="relative flex group items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+        <div className ="relative.min-h-[64px].min-w-[64px]" >
+            <Image
+                className="object-cover"
+                fill
+                src={image}
+                alt="Image"
+            />
+        </div>
+        <span className="font-medium truncate py-5">
+            {name}
+        </span>
+        <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+            <FaPlay className="text-black" />
+        </div>
+    </button>
   )
 }
