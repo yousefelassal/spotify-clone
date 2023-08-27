4eb2f7146db0a817af68d83e6bd1043b98963695
Yousef <yousefsameh24@gmail.com>
2023-08-27 19:16:09 +0300

    update list item

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -16,7 +16,7 @@ export default function ListItem({image, name, href}:{image: string, name: strin
         className="relative flex group items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
         onClick={onClick}
     >
-        <div className ="relative.min-h-[64px].min-w-[64px]" >
+        <div className ="relative min-h-[64px] min-w-[64px]" >
             <Image
                 className="object-cover"
                 fill
