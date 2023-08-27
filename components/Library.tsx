b6ebbe20cc0dc485a807c152e406f52c9954bad0
Yousef <yousefsameh24@gmail.com>
2023-08-27 16:31:03 +0300

    update library

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,5 +1,29 @@
+'use client'
+
+import { TbPlaylist } from 'react-icons/tb'
+import { AiOutlinePlus } from 'react-icons/ai'
+
 export default function Library() {
+  const onClick = () => {
+    //handle upload
+  }
+
   return (
-    <div>Library</div>
+    <div className="flex flex-col">
+        <div className="flex items-center justify-between px-5 pt-4">
+            <div className="inline-flex items-center gap-x-2">
+                <TbPlaylist size={26} className="text-neutral-400" />
+                <span className="text-neutral-400 text-md font-medium">Your Library</span>
+            </div>
+            <AiOutlinePlus 
+                onClick={onClick}
+                size={20}
+                className="text-neutral-400 cursor-pointer hover:text-white transition"
+            />
+        </div>
+        <div className="flex flex-col gap-y-2 mt-4 px-3">
+            List of Songs!
+        </div>
+    </div>
   )
 }
