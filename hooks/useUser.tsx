5eb660742a61efb61aceaba35cb78706f69890b8
Yousef <yousefsameh24@gmail.com>
2023-08-29 17:16:44 +0300

    update hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,6 +1,6 @@
 import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
 import { Subscription, UserDetails } from "@/types"
 
@@ -73,4 +73,12 @@ export const MyUserContextProvider = (props: Props) => {
     }
 
     return <UserContext.Provider value={value} {...props} />
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext)
+    if (context === undefined) {
+        throw new Error('useUser must be used within a UserContextProvider')
+    }
+    return context
 }
